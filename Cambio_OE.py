4d8c8862f75857177f26a8cad8276de8bd1162e6
EliasObreque <eliasobreque33@gmail.com>
2020-10-21 13:17:34 -0300

    Add Files

--- Cambio_OE.py
+++ Cambio_OE.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Aug  7 17:25:33 2018
-
 @author: Elias Obreque
 """
 
@@ -19,24 +17,28 @@ G           = 6.674e-11
 mu          = 3.986044418e5
 R_earth     = 6378.0000
 
-def JulianEpoch(y):
+
+def julianepoch(y):
     m   = 1
     J0  = 367.0*y - math.floor(7.0*(y + math.floor((m + 9.0)/12.0))/4.0) + math.floor(275.0*m/9.0) + 1721013.5
     return J0
 
-def Kepler_ite(M, e):
-    err     = 1.0e-10
-    E0      = M
-    t       = 1
-    itt     = 0
-    while(t):
-        E     =  M + e*np.sin(E0)
-        if (abs(E - E0) < err):
-            t = 0
-        E0    = E
-        itt   = itt + 1
-        E     = E0
-    return E 
+
+def kepler_ite(mm, ecc):
+    e_   = 0
+    err  = 1.0e-10
+    e0   = mm
+    flag = True
+    itt  = 0
+    while flag:
+        e_  = mm + ecc*np.sin(e0)
+        if abs(e_ - e0) < err:
+            flag = False
+        e0  = e_
+        itt = itt + 1
+        e_  = e0
+    return e_
+
 
 def getOE(Data):
     leng = len(Data)
@@ -62,7 +64,7 @@ def getOE(Data):
             a.append(((mu**(1.0/3.0))/(nsat**(2.0/3.0))))
             MA = float(Data[k][43:51])
             h = np.sqrt(mu*a[con]*(1 - e[con]**2))
-            E = Kepler_ite(MA, e[con])
+            E = kepler_ite(MA, e[con])
             f.append(2*np.arctan(np.sqrt((1.0 + e[con])/(1.0 - e[con]))*np.tan(0.5*E)))
             r.append(((h**2)/mu)/(1 + e[con]*np.cos(f[con])))
             con = con + 1
@@ -71,7 +73,7 @@ def getOE(Data):
                 year = float('20'+Data[k][18:20])
             else:
                 year = float('19'+Data[k][18:20])    
-            J0epoch = JulianEpoch(year)
+            J0epoch = julianepoch(year)
             epoch   = float(Data[k][20:32])
             Jepoch  = J0epoch + epoch
             beta.append(float(Data[k][53:59])*10**float(Data[k][59:61]))
@@ -85,7 +87,8 @@ def Trap(fx, x):
         area = area + 0.5*(x[i + 1] - x[i])*(fx[i + 1] + fx[i])
     return area
 
-TLE_open    = open(NombreTle+'.txt','r')
+
+TLE_open    = open(NombreTle+'.txt', 'r')
 TLE_read    = TLE_open.read()
 Data        = TLE_read.split('\n')
 TLE_open.close()
@@ -94,7 +97,7 @@ inc, a, n, e, RAAN, AP, tJD, r, f, beta = getOE(Data)
 
 t = (np.array(tJD) - tJD[0])
 
-NumRev = Trap(n,t)
+NumRev = Trap(n, t)
 
 KilTrav = 2*math.pi*(np.mean(a) + R_earth)*NumRev
 
---
+++ README.md
@@ -0,0 +1,11 @@
+# Historical Orbital Element Plotter
+
+These codes are created to analyze the historical orbital elements 
+for Cubesat satellites. 
+
+#
+The historical data are obtained from TLE files provided by NORAD and [Celestrack](https://celestrak.com/).
+
+***Contact***
+
+- els.obrq@gmail.com
